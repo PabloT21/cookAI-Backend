2a63d3dfdb572fffb2f5ebb31e0edc3dcbb4bb8d
Julian Berton <julianbertonb@gmail.com>
2025-12-20 15:07:51 -0300

    Modificado un poquitito main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 4000);
+
+  // Permitimos cors global, desp lo ajustamos
+  app.enableCors(); 
+
+  // Agregado el 0.0.0.0 para que el emulador de android lo pueda encontrar
+  await app.listen(process.env.PORT ?? 4000, '0.0.0.0');
+
+  // Agregado log
+  console.log(`Aplicacion corriendo en: ${await app.getUrl()}`);
 }
 bootstrap();
