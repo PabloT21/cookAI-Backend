579bb7e686bae9f22a67cb54296c9568ecb7f8a7
matiterrone <matiterrone@gmail.com>
2025-12-21 11:42:52 -0300

    userdb

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,7 +18,7 @@ import { RecipeModule } from './modules/recipe.module';
       type: 'mysql',
       host: 'localhost',
       port: 3306,
-      username: 'root',
+      username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
